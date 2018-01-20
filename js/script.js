8218b0a58f85025fead0077932fd32364c044de1
Ariel Biernacki <arielbiernacki@gmail.com>
2018-01-20 15:47:38 +0100

    Fix js problem

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ $(function() {
 
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
 
 function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
 };
 
 function createTweet(input) {
@@ -28,6 +30,8 @@ function createTweet(input) {
 	    $('.tweet').attr('href', tweet);
 	};
 }
+
+
 
     getQuote();
 
